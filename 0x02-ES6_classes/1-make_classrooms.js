308a2f7de46f1070f27b2fa34b0df9dddff2d944
yaekobbonke <yaekobbundure@gmail.com>
2023-09-29 19:23:35 +0300

    modified

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,9 +1,9 @@
 import {ClassRoom} from './0-classroom.js';
 
 function initializeRooms() {
-    room1 = new ClassRoom(19);
-    room2 = new ClassRoom(20);
-    room3 = new ClassRoom(34)
+    let room1 = new ClassRoom(19);
+    let room2 = new ClassRoom(20);
+    let room3 = new ClassRoom(34)
 
     return [room1, room2, room3];
 }
